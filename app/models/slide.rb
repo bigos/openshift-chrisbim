8248bb7f726f7ac8add78d6eabd543f5773a2c0b
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-10 23:10:09 +0100

    removed silly comment

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,7 +1,6 @@
 class Slide < ActiveRecord::Base
   attr_accessible :caption, :position, :visible, :photo
   
-  # will it work?
   has_attached_file :photo, :styles => { :thumb => "190x90>" }
  
   validates_presence_of :caption
