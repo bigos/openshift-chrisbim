23d88c5f3e56ca030771d74f25e082f3e3d734e7
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-10 02:01:54 +0100

    copied loads of files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,45 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require ckeditor/init
 //= require jquery
+//= require fancybox
 //= require jquery_ujs
+//= require jquery-ui
+//= require jquery-fileupload
 //= require_tree .
+
+
+jQuery(function() {
+  $("a.fancybox").fancybox();
+});
+
+jQuery(function() {
+  $("a.fancydiv").fancybox();
+});
+
+
+$(function() {
+    $( "#workshop_start_date" ).datepicker({
+	dateFormat: "dd/MM/yy",
+	firstDay: 1
+    });
+});
+
+
+function slideShow() {
+    var s=0;
+    max_slides = $('#image_container').data('slidecount');
+    interval = 5000;
+    setInterval( function(){
+	pos =  (s++ % max_slides) + 1;
+	nextpos =  (s % max_slides) + 1;
+	console.log(pos+'   '+nextpos)
+	$('#slide_'+pos).fadeOut();
+	$('#slide_'+nextpos).fadeIn();
+    }, interval);
+}
+
+$(document).ready(function(){
+    slideShow();    
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,41 @@
 module ApplicationHelper
+
+  def tagged_count(tag)
+    Photo.includes(:tags).where(:tags => {:name => tag.name}).count.to_s
+  end
+
+  def tag_li(tag, increase,level, max_level, type, show_count)
+    str =  "<li>"
+    str << link_to( tag.name + (show_count ? " #{tagged_count( tag)}" : ''), (type == :tags ? tag : photos_path(:tag => tag.name)))
+ 
+    str << recursive_tag_links(tag.name, level+increase, max_level, type, show_count).to_s
+    str << "</li>\n"
+  end
+
+  def recursive_tag_links(tag_name, level, max_level,type, show_count)
+    tag = Tag.where(:name=> tag_name).first
+    str = ''
+    if tag.children
+      if level <= max_level   
+        if tag.children.size > 0 
+          str << "<ul class=\"depth-#{level}\"> "
+          tag.children.sort_by(& :name).each do |tag|
+            str << tag_li(tag, 1, level, max_level,type, show_count)
+          end
+          str << '</ul>'
+        end
+      end
+    end
+  end
+
+  def tag_links( max_level = 1000, type = :tags, show_count = false)
+    top_tags = Tag.where(:parent_id => nil).order(:name).all
+    level = 1
+    str = ''    
+    str << "<ul class=\"depth-0\">"
+    top_tags.each do |tag|
+      str << tag_li(tag, 0, level, max_level, type, show_count)
+    end
+    str << "</ul>"
+  end
 end
